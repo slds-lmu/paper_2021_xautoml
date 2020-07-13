79468ee38072c93ae1cfab7028305d87fca6a680
JuliaHerbinger <42943743+JuliaHerbinger@users.noreply.github.com>
2020-07-13 12:58:22 +0200

    create files to read in data and use iml methods on top

---
+++ R/data_prep.r
@@ -0,0 +1,26 @@
+# create data lists
+data_btss = list(
+  "mbo_lambda0.5" = readRDS("data/BTSS/mbo_lambda0.5.rds"),
+  "mbo_lambda1.0" = readRDS("data/BTSS/mbo_lambda1.rds"),
+  "mbo_lambda2.0" = readRDS("data/BTSS/mbo_lambda2.rds"),
+  "test" = readRDS("data/BTSS/randomLHS_1000.rds")
+)
+
+data_kc1 = list(
+  "mbo_lambda0.5" = readRDS("data/kc1/mbo_lambda0.5.rds"),
+  "mbo_lambda1.0" = readRDS("data/kc1/mbo_lambda1.rds"),
+  "mbo_lambda2.0" = readRDS("data/kc1/mbo_lambda2.rds"),
+  "test" = readRDS("data/kc1/randomLHS_1000.rds")
+)
+
+get_objects <- function(data, lambda){
+  objectname = paste0("mbo_lambda", lambda)
+  object = data[[objectname]]
+  model = object$models$models
+  data_train = object$tasks[[1]]$env$data
+  data_test = data$test
+  return(list("model" = model, "data_train" = data_train, "data_test" = data_test))
+}
+
+
+#test = get_objects(data = data_btss, lambda = 0.5)
