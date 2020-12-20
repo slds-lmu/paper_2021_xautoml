ce99289de5cb41388753d18224fb09b711c02578
juliambr <ju.moosbauer@googlemail.com>
2020-12-20 19:31:10 +0100

    cleaning

--- testfiles/test_tree_splitting.R
+++ testfiles/test_tree_splitting.R
@@ -8,9 +8,9 @@ library(Rfast)
 library(ggplot2)
 library(gridExtra)
 
-source("notebooks/mbo_helpers.R")
-source("notebooks/pdp_helpers.R")
-source("notebooks/tree_splitting.R")
+source("R/pdp_helpers.R")
+source("R/tree_splitting.R")
+source("R/benchmarks/synthetic/mbo_helpers.R")
 
 # Objective function which was initially analyzed
 obj = makeSingleObjectiveFunction(name = "StyblinkskiTang3D", fn = function(x) {
@@ -21,10 +21,13 @@ obj = makeSingleObjectiveFunction(name = "StyblinkskiTang3D", fn = function(x) {
 ps = getParamSet(obj)
 
 # Read in models for interpretation
-runs = list(MBO_0.1 = readRDS("synthetic/mlrmbo_run_StyblinkskiTang3D_01.rds"), 
-            MBO_1 = readRDS("synthetic/mlrmbo_run_StyblinkskiTang3D_1.rds"),
-            MBO_2 = readRDS("synthetic/mlrmbo_run_StyblinkskiTang3D_2.rds")
+runs = list(MBO_0.1 = readRDS("data/runs/synthetic/mlrmbo_run_StyblinkskiTang2D_01.rds"), 
+            MBO_1 = readRDS("data/runs/synthetic/mlrmbo_run_StyblinkskiTang2D_1.rds"),
+            MBO_2 = readRDS("data/runs/synthetic/mlrmbo_run_StyblinkskiTang2D_2.rds")
         )
+
+models = extract_models(runs)
+names(models) = get_types_of_runs(runs)
 
 # Choose one model to interpret
 model_for_interpretation = "MBO_1"
@@ -33,16 +36,8 @@ model = models[[model_for_interpretation]]
 # Testdata 
 df = generateDesign(par.set = ps, n = 1000, fun = lhs::randomLHS)
 
-# Compute ICE curves as 
-mymodel = makeS3Obj("mymodel", fun = function() return(model))
-
-predict.mymodel = function(object, newdata) {
-  pred = predict(object$fun(), newdata = newdata)
-  getPredictionSE(pred) 
-}
-
-predictor = Predictor$new(mymodel, data = df, predict.function = predict.mymodel)
-effect = FeatureEffect$new(predictor, method = "ice", grid.size = 20, feature = "x1")
+predictor = Predictor$new(model = model, data = df[c("x1", "x2")])
+effect = FeatureEffect$new(predictor = predictor, feature = "x1", method = "ice")
 
 
 # define objective
@@ -61,13 +56,15 @@ get_size_of_tree(tree)
 get_objective_values(tree)
 
 # Plot node 2 in depth 3
+
+depth = 3
 node = tree[[depth]][[2]]
-plot_pdp_for_node(node, model, "x1", objective.groundtruth = obj1)
+plot_pdp_for_node(node, model, "x1", objective.groundtruth = obj)
 
 lapply(seq_len(length(tree) - 1), function(depth) {
   nodes = tree[[depth]]
   plist = lapply(nodes, function(node) {
-    plot_pdp_for_node(node, model, "x1", objective.groundtruth = obj1)
+    plot_pdp_for_node(node, model, "x1", objective.groundtruth = obj)
   })
   do.call(grid.arrange, c(plist, nrow = 1))
 })
